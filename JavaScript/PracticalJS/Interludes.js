b072a5092566d2a39b2ad6e08a16eaa99d7a517a
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2019-10-11 22:42:19 +0400

    Oct 11

---
+++ JavaScript/PracticalJS/Interludes.js
@@ -0,0 +1,63 @@
+// Inside of a function -> you can look out and see data
+// Outside of a function -> you can't look in
+
+// draw arrow from where each variable is being accessed inside function -> 
+// can't go into circle (that's around the function)
+
+// For Primatives - values are compared - 'passing by value'
+// For Objects - references(memory addresses) are compared - 'passing by reference'
+
+// The browsers interpretation of your HTML is the DOM
+
+
+
+// Arrays
+// It should have a place to store todos
+// It should have a way to display todos
+// It should have a way to add new todos
+// It should have a way to change a todo
+// It should have a way to delete a todo
+
+// Functions
+// It should have a function to display todos
+// It should have a function to add new todos
+// It should have a function to change a todo
+// It should have a function to delete a todo
+
+// Objects
+// It should store the todos array on an object
+// It should have a displayTodos method
+// It should have an addTodo method
+// It should have a changeTodo method
+// It should have a deleteTodo method
+
+// Booleans
+// todoList.addTodo should add objects
+// todoList.changeTodo should change the todoText property
+// todoList.toggleCompleted should flip the completed property
+
+// Loops
+// displayTodos should show .todoText
+// displayTodos should tell you if .todos is empty
+// displayTodos should show .completed
+
+// Toggle
+// .toggleAll: If everything's true, make everything false
+
+// DOM
+// Clicking “Display todos” should run todoList.displayTodos
+
+
+// Version 8 - Getting data from inputs
+// There should be a button for adding todos
+// There should be a button for changing todos
+// There should be a button for deleting todos
+// There should be a button for toggling a todo
+
+
+// v9 Escape form Console
+// There should be an li element for every todo
+// Each li element should contain .todoText
+// Each li element should show .completed
+
+
