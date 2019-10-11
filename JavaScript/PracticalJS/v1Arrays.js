8eaf86ec130deffb3a0d4be51312611a369767f9
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2019-10-11 22:35:42 +0400

    Oct 11

---
+++ JavaScript/PracticalJS/v1Arrays.js
@@ -0,0 +1,18 @@
+// It should have a place to store todos
+var todos = ["item 1", "item 2", "item 3"]
+todos
+
+// It should have a way to display todos
+console.log(todos)
+
+// It should have a way to add new todos
+todos.push("item 4")
+todos
+
+// It should have a way to change a todo
+todos[0] = "item 1 updated"
+todos
+
+// It should have a way to delete a todo
+todos.splice(1,1)
+todos
